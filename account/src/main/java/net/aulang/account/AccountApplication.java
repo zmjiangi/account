82f83e94a1feeaa2fb3285e3f20e5fdaef735755
Aulang <aulang@qq.com>
2017-08-07 23:36:53 +0800

    完善

--- account/src/main/java/net/aulang/account/AccountApplication.java
+++ account/src/main/java/net/aulang/account/AccountApplication.java
@@ -2,7 +2,10 @@ package net.aulang.account;
 
 import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.MetricsDropwizardAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.jersey.JerseyAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -15,8 +18,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableScheduling
 @EnableDiscoveryClient
-@SpringBootApplication
-@ComponentScan("net.aulang.account")
+@SpringBootApplication(exclude = {
+		JerseyAutoConfiguration.class,
+		GroovyTemplateAutoConfiguration.class,
+		MetricsDropwizardAutoConfiguration.class})
+@ComponentScan({"net.aulang.account", "org.apereo.cas"})
 @EnableConfigurationProperties(CasConfigurationProperties.class)
 @ImportResource(locations = { "classpath:/deployerConfigContext.xml", "classpath:/deployerConfigContext.groovy" })
 public class AccountApplication extends SpringBootServletInitializer {
