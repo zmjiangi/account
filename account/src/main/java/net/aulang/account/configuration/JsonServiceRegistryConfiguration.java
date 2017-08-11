3999242acda7dfccfbfd96f177de7b8599aefdd5
Aulang <aulang@qq.com>
2017-08-11 14:50:49 +0800

    更新

---
+++ account/src/main/java/net/aulang/account/configuration/JsonServiceRegistryConfiguration.java
@@ -0,0 +1,35 @@
+package net.aulang.account.configuration;
+
+import org.apereo.cas.services.ServiceRegistryDao;
+import org.apereo.cas.services.ServicesManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class JsonServiceRegistryConfiguration {
+    @Autowired
+    @Qualifier("servicesManager")
+    private ServicesManager servicesManager;
+
+    @Autowired
+    @Qualifier("serviceRegistryDao")
+    private ServiceRegistryDao serviceRegistryDao;
+
+    @Autowired
+    @Qualifier("embeddedJsonServiceRegistry")
+    private ServiceRegistryDao embeddedJsonServiceRegistry;
+
+    /**
+     * 添加了OAuth支持后，CAS不读取json文件，需要手动配置
+     */
+    @PostConstruct
+    public void init() {
+        this.embeddedJsonServiceRegistry.load().forEach(e -> {
+            this.serviceRegistryDao.save(e);
+        });
+        servicesManager.load();
+    }
+}
