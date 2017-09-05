d2636e62b09c87f88249996e7c54fa8d8479783f
Aulang <aulang@qq.com>
2017-09-05 23:19:01 +0800

    注册中心设置

--- account/src/main/java/net/aulang/account/AccountApplication.java
+++ account/src/main/java/net/aulang/account/AccountApplication.java
@@ -8,9 +8,7 @@ import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAuto
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jersey.JerseyAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
@@ -32,13 +30,8 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @ComponentScan({"net.aulang.account", "org.apereo.cas"})
 @EnableConfigurationProperties(CasConfigurationProperties.class)
 @ImportResource(locations = {"classpath:/deployerConfigContext.xml", "classpath:/deployerConfigContext.groovy"})
-public class AccountApplication extends SpringBootServletInitializer {
-    public static void main(final String[] args) {
+public class AccountApplication {
+    public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
-        return builder.sources(AccountApplication.class);
     }
 }
