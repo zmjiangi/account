bdabe36a90e6a67a452380dd0f89e95959049ef1
Aulang <aulang@qq.com>
2017-11-23 12:21:30 +0800

    去除CAS

--- account/src/main/java/net/aulang/account/AccountApplication.java
+++ account/src/main/java/net/aulang/account/AccountApplication.java
@@ -1,33 +1,11 @@
 package net.aulang.account;
 
-import org.apereo.cas.configuration.CasConfigurationProperties;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.MetricsDropwizardAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jersey.JerseyAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAsync
-@EnableScheduling
 @EnableDiscoveryClient
-@SpringBootApplication(exclude = {
-        JerseyAutoConfiguration.class,
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class,
-        GroovyTemplateAutoConfiguration.class,
-        MetricsDropwizardAutoConfiguration.class
-})
-@ComponentScan({"net.aulang.account", "org.apereo.cas"})
-@EnableConfigurationProperties(CasConfigurationProperties.class)
-@ImportResource(locations = {"classpath:/deployerConfigContext.xml", "classpath:/deployerConfigContext.groovy"})
+@SpringBootApplication(scanBasePackages = "net.aulang.account")
 public class AccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
--- account/src/main/java/net/aulang/account/factory/HttpsURLConnectionFactory.java
+++ account/src/main/java/net/aulang/account/factory/HttpsURLConnectionFactory.java
@@ -5,32 +5,18 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
 import org.apache.http.ssl.TrustStrategy;
-import org.jasig.cas.client.ssl.HttpURLConnectionFactory;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
-import java.net.HttpURLConnection;
-import java.net.URLConnection;
 import java.security.cert.X509Certificate;
 
 /**
  * HTTP连接工厂，忽略HTTPS证书
  */
-public class HttpsURLConnectionFactory implements HttpURLConnectionFactory {
-    @Override
-    public HttpURLConnection buildHttpURLConnection(URLConnection conn) {
-        if (conn instanceof HttpsURLConnection) {
-            HttpsURLConnection httpsConnection = (HttpsURLConnection) conn;
-            httpsConnection.setSSLSocketFactory(sslSocketFactory());
-            httpsConnection.setHostnameVerifier(hostnameVerifier());
-        }
-        return (HttpURLConnection) conn;
-    }
-
+public class HttpsURLConnectionFactory {
     public static ClientHttpRequestFactory clientHttpRequestFactory() {
         return new HttpComponentsClientHttpRequestFactory(httpClient());
     }
