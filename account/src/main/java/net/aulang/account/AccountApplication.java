c239b2a8ca5043ab194170bd083724ec460edf8d
Aulang <aulang@qq.com>
2018-01-15 23:22:14 +0800

    配置更新

--- account/src/main/java/net/aulang/account/AccountApplication.java
+++ account/src/main/java/net/aulang/account/AccountApplication.java
@@ -3,8 +3,11 @@ package net.aulang.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@EnableDiscoveryClient
+@EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "net.aulang.account")
 public class AccountApplication {
     public static void main(String[] args) {
--- account/src/main/java/net/aulang/account/restcontroller/AccountRestController.java
+++ account/src/main/java/net/aulang/account/restcontroller/AccountRestController.java
@@ -1,24 +1,22 @@
 package net.aulang.account.restcontroller;
 
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
 import net.aulang.account.manage.AccountBiz;
 import net.aulang.account.model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class InitialRestController {
+@RequestMapping("/account")
+public class AccountRestController {
     @Autowired
     private AccountBiz accountBiz;
 
-    @ApiOperation("初始化用户信息")
-    @ApiImplicitParam(name = "name", value = "初始化账号名", required = true, paramType = "query", dataType = "String")
-    @ApiResponse(code = 200, message = "success：成功；fail：失败")
-    @GetMapping(path = "/inituser", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @GetMapping(path = "/init", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String init(String name) {
         if ("aulang".equals(name)) {
             long count = accountBiz.count();
@@ -36,6 +34,17 @@ public class InitialRestController {
             }
         }
         return "fail";
+    }
+
+    @GetMapping(path = "/{name}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Account> getByName(@PathVariable String name) {
+        Account account = accountBiz.getByLoginName(name);
+
+        if (account == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(account);
+        }
     }
 
 }
